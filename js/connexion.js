7f85a4b83727cb17969069b58127830f65719a47
Narvalo17 <102968094+Narvalo17@users.noreply.github.com>
2024-06-05 14:55:43 +0200

    regex

--- js/connexion.js
+++ js/connexion.js
@@ -1,4 +1,4 @@
-import { BASE_URL,PORT, LOGIN, USER_ENDPOINT } from "./constantes.js"
+/* import { BASE_URL,PORT, LOGIN, USER_ENDPOINT } from "./constantes.js"
 
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
     event.preventDefault(); 
@@ -39,4 +39,58 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
       }
       document.getElementById('logout').addEventListener('click', function() {
           localStorage.removeItem('userData');
-      });
+      }); */
+      import { BASE_URL, PORT, LOGIN, USER_ENDPOINT } from "./constantes.js";
+
+const regexEmail = /^[a-zA-Z0-9._%+-]+@(gmail|yahoo|outlook|hotmail)\.(com|fr|net)$/;
+const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+*/=])[A-Za-z\d\-+*/=]{8,}$/;
+
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); 
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (!regexEmail.test(email)) {
+        console.alert("Erreur: L'adresse e-mail n'est pas valide.");
+        return; // Arrête l'exécution si l'adresse e-mail n'est pas valide
+    }
+
+    if (!regexPassword.test(password)) {
+        console.alert("Erreur: Le mot de passe n'est pas conforme.");
+        return; // Arrête l'exécution si le mot de passe n'est pas conforme
+    }
+
+    let formData = {
+        email: email,
+        motDePasse: password,
+    };
+
+    console.log(formData);
+    
+    try {
+        const response = await axios.post(`${BASE_URL}${PORT}${USER_ENDPOINT}${LOGIN}`, {
+            ...formData
+        });
+
+        console.log(response);
+        localStorage.setItem('userData', JSON.stringify(response.data));
+        console.log('Données utilisateur stockées dans le localStorage'); 
+
+        window.location.href = '../aprescnx/accueil.html';
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+let userData = localStorage.getItem('userData');
+if (userData) {
+    userData = JSON.parse(userData);
+    document.getElementById('nom-utilisateur').innerHTML = `Bonjour, ${JSON.stringify(userData.userApp.nom)} <a href="../index.html" id="logout">Se déconnecter</a>`;
+} else {
+    document.getElementById('nom-utilisateur').textContent = "Message d'erreur approprié en cas d'absence de données utilisateur";
+}
+
+document.getElementById('logout').addEventListener('click', function() {
+    localStorage.removeItem('userData');
+}); 
--- js/inscription.js
+++ js/inscription.js
@@ -1,4 +1,4 @@
-import { BASE_URL,PORT, PRO_ENDPOINT, USER_ENDPOINT } from "./constantes.js"
+/* import { BASE_URL,PORT, PRO_ENDPOINT, USER_ENDPOINT } from "./constantes.js"
 
 document.getElementById('userInput').addEventListener('change', function() {
     let user = this.value;
@@ -54,3 +54,74 @@ document.getElementById('inscriptionForm').addEventListener('submit', async func
       }
     });
 
+ */
+
+    import { BASE_URL, PORT, PRO_ENDPOINT, USER_ENDPOINT } from "./constantes.js";
+
+const regexEmail = /^[a-zA-Z0-9._%+-]+@(gmail|yahoo|outlook|hotmail)\.(com|fr|net)$/;
+const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+*/=])[A-Za-z\d\-+*/=]{8,}$/;
+
+
+document.getElementById('userInput').addEventListener('change', function() {
+    let user = this.value;
+    if (user === 'Professionnel') {
+        document.getElementById('professionnelleForm').style.display = 'block';
+    } else {
+        document.getElementById('professionnelleForm').style.display = 'none';
+    }
+});
+
+document.getElementById('inscriptionForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); 
+
+    let userType = document.getElementById('userInput').value;
+    let nom, prenom, email, password, confirmation, raison, siret;
+    
+    if (userType === 'Client') {
+        nom = document.getElementById('nom').value;
+        prenom = document.getElementById('prenom').value;
+        email = document.getElementById('email').value;
+        password = document.getElementById('password').value;
+        confirmation = document.getElementById('confirmation').value;
+    } else {
+        raison = document.getElementById('raison').value;
+        siret = document.getElementById('siret').value;
+        nom = prenom = email = password = confirmation = null; // Reset des valeurs pour le cas où
+    }
+
+    if (password !== confirmation) {
+        console.log("Erreur: Le mot de passe et sa confirmation ne correspondent pas.");
+        return; // Arrête l'exécution si le mot de passe et sa confirmation ne correspondent pas
+    }
+
+    if (!regexEmail.test(email)) {
+        console.log("Erreur: L'adresse e-mail n'est pas valide.");
+        return; // Arrête l'exécution si l'adresse e-mail n'est pas valide
+    }
+
+    if (!regexPassword.test(password)) {
+        console.log("Erreur: Le mot de passe n'est pas conforme.");
+        return; // Arrête l'exécution si le mot de passe n'est pas conforme
+    }
+
+    let formData = {
+        nom: nom,
+        prenom: prenom,
+        email: email,
+        motDePasse: password,
+        raisonSociale: raison,
+        siret: siret
+    };
+
+    console.log(formData);
+    try {
+        const response = await axios.post(`${BASE_URL}${PORT}${userType === "Client" ? USER_ENDPOINT : PRO_ENDPOINT}`, {
+            ...formData
+        });
+    
+        console.log('Réponse du serveur :', response.data);
+        console.log("Utilisateur ajouté avec succès.");
+    } catch (error) {
+        console.error('Erreur lors de la requête :', error);
+    }
+});
